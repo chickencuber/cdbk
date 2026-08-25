91294d99de4810a5ce22625cf2958150c434a11e
chickencuber <n1batman728@gmail.com>
2026-08-24 22:47:19 -0500

    made it use qt theme

--- build.rs
+++ build.rs
@@ -1,3 +1,8 @@
 fn main() {
-    slint_build::compile("./src/resources/ui/window.slint").unwrap();
+    // slint_build::compile("./src/resources/ui/window.slint").unwrap();
+    slint_build::compile_with_config(
+        "./src/resources/ui/window.slint",
+        slint_build::CompilerConfiguration::new().with_style("native".to_string()),
+    )
+    .unwrap();
 }
